2b1b538c5321247145d7e585df57c60529e24338
Abhilash-Bee <abhilashjanarrdhan@gmail.com>
2022-09-15 13:09:07 +0530

    Advanced Blog Capstone updates

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+USER = os.getenv("REACT_APP_USER_ID")
+PASSWORD = os.getenv("REACT_APP_PASSWORD")
 
 app = Flask(__name__)
 
@@ -27,9 +35,29 @@ def post(num):
     return render_template("post.html", requested_post=requested_post)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        name, email, phone, message = data["username"], data["email"], data["phone"], data["message"]
+        if name != "" and email != "" and phone != "" and message != "":
+            mail_to(name, email, phone, message)
+            return render_template("contact.html", is_mail="sent")
+        else:
+            return render_template("contact.html", is_mail="nsent")
+    return render_template("contact.html", is_mail="False")
+
+
+def mail_to(name, email, phone, message):
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=USER, password=PASSWORD)
+        connection.sendmail(
+            from_addr=USER,
+            to_addrs="8bjabhilash@gmail.com",
+            msg=f"Subject: Mail from {name} to your blog\n\n"
+                f"Message: {message}\nEmail id: {email}\nPhone no: {phone}"
+        )
 
 
 if __name__ == "__main__":
